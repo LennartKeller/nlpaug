ba945ec597763f04077072e67d6ab3ec3420f3ba
Lennart Keller <lennart.keller@uni-wuerzburg.de>
2021-05-19 12:42:58 +0200

    remove paper ref

--- nlpaug/model/lang_models/xlm_roberta.py
+++ nlpaug/model/lang_models/xlm_roberta.py
@@ -12,7 +12,6 @@ from nlpaug.util.selection.filtering import *
 
 
 class XLMRoberta(Roberta):
-    # https://arxiv.org/pdf/1907.11692.pdf
     START_TOKEN = '<s>'
     SEPARATOR_TOKEN = '</s>'
     MASK_TOKEN = '<mask>'
